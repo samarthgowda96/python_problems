b9b50d535ebd3d25e6eae63e24a1e12d148a5335
Paulina Khew <paulinakhew12345@hotmail.com>
2019-07-03 18:18:42 -0400

    :sparkles: Add bubble sort algorithm

---
+++ move_the_zeroes/zeroes.py
@@ -0,0 +1,11 @@
+def move_zeroes(list):
+    n = len(list)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if list[j] == 0:
+                list[j], list[j+1] = list[j+1], list[j]
+    print(list)
+
+x = [0,1,0,3,12]
+
+move_zeroes(x)
---
+++ schema_designer/schema_designer.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+connection = sqlite3.connect('master.db',check_same_thread=False)
+cursor = connection.cursor()
+
+cursor.execute(
+    """CREATE TABLE states(
+        state_name TEXT PRIMARY KEY
+    );"""
+)
+
+cursor.execute(
+    """CREATE TABLE cities(
+        city_name TEXT PRIMARY KEY,
+        state TEXT,
+        FOREIGN KEY(state) REFERENCES states(state_name)
+    );"""
+)
+
+cursor.execute(
+    """CREATE TABLE parks(
+        park_name TEXT PRIMARY KEY,
+        state TEXT,
+        FOREIGN KEY(state) REFERENCES states(state_name)
+    );"""
+)
+
+cursor.execute(
+    """CREATE TABLE doctors(
+        doctor_name TEXT PRIMARY KEY
+    );"""
+)
+
+cursor.execute(
+    """CREATE TABLE patients(
+        patient_name TEXT PRIMARY KEY
+    );"""
+)
+
+cursor.execute(
+    """CREATE TABLE doctors_patients(
+        patient TEXT,
+        doctor TEXT,
+        FOREIGN KEY(patient) REFERENCES patients(patient_name),
+        FOREIGN KEY(doctor) REFERENCES doctors(doctor_name)
+    );"""
+)
+
+cursor.execute(
+    """CREATE TABLE users_and_admin(
+        name
+        username
+        password
+        email
+        phone_number
+
+
+
+
+cursor.close()
+connection.close()
+
+
+
