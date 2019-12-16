4c3d7d36fa2fa456211a79ab05aa01f367430f9e
Paulina Khew <paulinakhew12345@hotmail.com>
2019-12-15 20:50:15 -0500

    :rotating_light: Fix linter warnings and remove pytest from yaml file

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -13,7 +13,7 @@ Output: [1,2,2,3,5,6]
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: [int], m: int, nums2: [int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
