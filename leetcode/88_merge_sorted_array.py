74ba1fe0758b6883fb52fca271ba340b750519d2
Paulina Khew <paulinakhew12345@hotmail.com>
2019-12-15 21:16:02 -0500

    :sparkles: Add pow x n problem

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -1,7 +1,9 @@
-'''Description
+'''88. Merge Sorted Array Description
 Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-The number of elements initialized in nums1 and nums2 are m and n respectively.
-You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+
+Note:
+- The number of elements initialized in nums1 and nums2 are m and n respectively.
+- You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
 
 Example:
 Input:
