8f97248b3e58e7f16502e86735c699e24bea1fde
Paulina Khew <paulinakhew12345@hotmail.com>
2020-01-18 14:07:42 -0500

    :sparkles: Add solution for Sort Array by Parity

--- leetcode/1281_subtract_the_product_and_sum_of_digits_of_an_integer.py
+++ leetcode/1281_subtract_the_product_and_sum_of_digits_of_an_integer.py
@@ -3,9 +3,9 @@ Given an integer number n, return the difference between the product of its digi
 
 Example 1:
 Input: n = 234
-Output: 15 
-Explanation: 
-Product of digits = 2 * 3 * 4 = 24 
+Output: 15
+Explanation:
+Product of digits = 2 * 3 * 4 = 24
 Sum of digits = 2 + 3 + 4 = 9
 Result = 24 - 9 = 15
 
---
+++ leetcode/905_sort_array_by_parity.py
@@ -0,0 +1,32 @@
+'''905. Sort Array By Parity Description
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+
+Example 1:
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Note:
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+'''
+
+
+class Solution:
+    def sortArrayByParity(self, A: [int]) -> [int]:
+        even = []
+        odd = []
+        for number in A:
+            if number % 2 == 0:
+                even.append(number)
+            else:
+                odd.append(number)
+        return even + odd
+
+
+'''Submission
+Runtime: 84 ms, faster than 47.58% of Python3 online submissions for Sort Array By Parity.
+Memory Usage: 13.4 MB, less than 98.70% of Python3 online submissions for Sort Array By Parity.
+'''
