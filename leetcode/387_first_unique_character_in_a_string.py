35c837e4009466c450cdb64d04400da7903011f4
Paulina Khew <paulinakhew12345@hotmail.com>
2020-01-28 16:20:55 -0500

    :sparkles: Add solution for first unique character in a string

---
+++ leetcode/387_first_unique_character_in_a_string.py
@@ -0,0 +1,28 @@
+'''387. First Unique Character in a String Description
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+
+Note: You may assume the string contain only lowercase letters.'''
+
+
+import collections
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        c = collections.Counter(s)
+        for letter in sorted(set(s), key=s.index):
+            if c[letter] == 1:
+                return s.index(letter)
+        return -1
+
+
+'''Submission
+Runtime: 72 ms, faster than 92.22% of Python3 online submissions for First Unique Character in a String.
+Memory Usage: 13 MB, less than 100.00% of Python3 online submissions for First Unique Character in a String.'''
