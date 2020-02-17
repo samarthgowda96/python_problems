77ef9a2ea1d504e1abc70597985af44ad8002e09
Paulina Khew <paulinakhew12345@hotmail.com>
2020-02-17 15:28:29 -0500

    :sparkles: Add solution for linked list cycle

---
+++ leetcode/141_linked_list_cycle.py
@@ -0,0 +1,45 @@
+'''141. Linked List Cycle Description
+
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
+If pos is -1, then there is no cycle in the linked list.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        while (head):
+            if head.val is None:
+                return True
+            head.val = None
+            head = head.next
+        return False
+
+
+'''Submission
+Runtime: 44 ms, faster than 85.80% of Python3 online submissions for Linked List Cycle.
+Memory Usage: 15.8 MB, less than 100.00% of Python3 online submissions for Linked List Cycle.'''
