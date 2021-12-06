cb51db5b3a4ac9c701890534816e11d8e72122f6
Paulina Khew <paulinakhew12345@hotmail.com>
2021-12-06 18:19:01 -0500

    :tada: Add solution to advent of code day 1 part 1

---
+++ advent_of_code/2021/day_1/puzzle_1.py
@@ -0,0 +1,12 @@
+f = open("puzzle_1_input.txt", "r")
+
+nums = f.readlines()
+nums = [int(i) for i in nums]
+
+count = 0
+
+for i in range(1, len(nums)):
+    if nums[i - 1] < nums[i]:
+        count += 1
+
+print(count)
