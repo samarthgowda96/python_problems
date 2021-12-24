99ebba9bf0e3d4b8313f9b7f6c338b0797b405bd
Paulina Khew <paulinakhew12345@hotmail.com>
2021-12-23 22:10:08 -0500

    :art: Make code more efficient

--- advent_of_code/2021/day_6/part_1.py
+++ advent_of_code/2021/day_6/part_1.py
@@ -4,16 +4,14 @@ f = open("input.txt", "r")
 lantern_fish = [int(x) for x in f.readlines()[0].split(",")]
 
 for i in range(80):
-    new_fish = 0
+    new_fish = []
     for i in range(len(lantern_fish)):
         lantern_fish[i] -= 1
 
-    for i in range(len(lantern_fish)):
         if lantern_fish[i] == -1:
             lantern_fish[i] = 6
-            new_fish += 1
+            new_fish.append(8)
 
-    for i in range(new_fish):
-        lantern_fish.append(8)
+    lantern_fish = lantern_fish + new_fish
 
 print(len(lantern_fish))
