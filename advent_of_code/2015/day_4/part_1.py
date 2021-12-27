900025be3a0fb80dd9f1b8ba0b7411b71170bf6d
Paulina Khew <paulinakhew12345@hotmail.com>
2021-12-26 22:33:40 -0500

    :sparkles: Add solution to aoc 2015 day 4 part 1

---
+++ advent_of_code/2015/day_4/part_1.py
@@ -0,0 +1,13 @@
+import hashlib
+
+f = open("input.txt", "r")
+
+secret_key = f.readlines()[0]
+hash = ""
+i = 1
+
+while hash[:5] != "00000":
+    i += 1
+    hash = hashlib.md5(f"{secret_key}{i}".encode()).hexdigest()
+
+print(i)
