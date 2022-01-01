581ddd54f3b8a90b5a67b983b17a04f96ea51225
Paulina Khew <paulinakhew12345@hotmail.com>
2022-01-01 14:22:19 -0500

    :sparkles: Add solution to aoc 2021 day 10 part 1

---
+++ advent_of_code/2021/day_10/part_1.py
@@ -0,0 +1,31 @@
+f = open("input.txt", "r")
+lines = [x.strip("\n") for x in f.readlines()]
+
+opening_brackets = ["(", "[", "{", "<"]
+closing_brackets = [")", "]", "}", ">"]
+
+my_stack = []
+total = 0
+corrupt = False
+
+for line in lines:
+    for char in line:
+        if char in opening_brackets:
+            my_stack.append(opening_brackets.index(char))
+        else:
+            closing_brackets_index = closing_brackets.index(char)
+            if closing_brackets_index == my_stack[-1]:
+                my_stack.pop()
+            else:
+                if closing_brackets_index == 0:
+                    total += 3
+                elif closing_brackets_index == 1:
+                    total += 57
+                elif closing_brackets_index == 2:
+                    total += 1197
+                else:
+                    total += 25137
+                corrupt = True
+                break
+
+print(total)
