31215450470d6e98db601f0cb0b2b145b1355607
Paulina Khew <paulinakhew12345@hotmail.com>
2022-06-25 23:37:57 -0700

    :art: Update search insert position problem

--- leetcode/35_search_insert_position.py
+++ leetcode/35_search_insert_position.py
@@ -1,36 +1,42 @@
 """35. Search Insert Position
-Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-You may assume no duplicates in the array.
+You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
-Input: [1,3,5,6], 5
+Input: nums = [1,3,5,6], target = 5
 Output: 2
 
 Example 2:
-Input: [1,3,5,6], 2
+Input: nums = [1,3,5,6], target = 2
 Output: 1
 
 Example 3:
-Input: [1,3,5,6], 7
+Input: nums = [1,3,5,6], target = 7
 Output: 4
 
-Example 4:
-Input: [1,3,5,6], 0
-Output: 0"""
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104"""
 
 
 class Solution:
-    def searchInsert(self, nums: [int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
 
-        for i in range(len(nums)):
-            if target < nums[i]:
-                return i
-        return len(nums)
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
 
 
 """Submission
-Runtime: 48 ms, faster than 89.24% of Python3 online submissions for Search Insert Position.
-Memory Usage: 14.6 MB, less than 22.65% of Python3 online submissions for Search Insert Position."""
+Runtime: 81 ms, faster than 37.14% of Python3 online submissions for Search Insert Position.
+Memory Usage: 14.6 MB, less than 98.86% of Python3 online submissions for Search Insert Position."""
